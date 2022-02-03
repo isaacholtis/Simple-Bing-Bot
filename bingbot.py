cd2b8672596d4b760c89cebea27cae62ea363aa5
Isaac Holt <holtimzws@outlook.com>
2022-02-03 16:40:57 -0700

    Add non-functional code, just in case I want to modify later

--- bingbot.py
+++ bingbot.py
@@ -2,7 +2,7 @@ import webbrowser
 import random
 
 searchNum = int(input("Please input the number of tabs you want to open.\nPlease note that the more tabs you open the more ram you will use, and we are not responsible for your computer crashing.\n-->"))
-
+timesNum = int(input("Please input the number of times you would like to run this.\nie your inputed amount of tabs, times whatever number you input now. The max number of times is 4.\n-->"))
 
 num = 0
 webLink = []
@@ -10,4 +10,36 @@ for i in range(searchNum):
     webLink.append(f"https://www.bing.com/search?q={(i + 1)}")
 for i in range(searchNum):
     webbrowser.open (webLink[i])
-print(webLink)
+
+if timesNum == 0:
+    for i in range(searchNum):
+        webLink.append(f"https://www.bing.com/search?q={(i)}")
+    for i in range(searchNum):
+        webbrowser.open (webLink[i])
+
+elif timesNum == 1:
+    for i in range(searchNum + 1):
+        webLink.append(f"https://www.bing.com/search?q={(i + 2)}")
+    for i in range(searchNum + 1):
+        webbrowser.open (webLink[i])
+
+elif timesNum == 2:
+    for i in range(searchNum + 2):
+        webLink.append(f"https://www.bing.com/search?q={(i + 3)}")
+    for i in range(searchNum + 2):
+        webbrowser.open (webLink[i])
+
+elif timesNum == 3:
+    for i in range(searchNum + 3):
+        webLink.append(f"https://www.bing.com/search?q={(i + 4)}")
+    for i in range(searchNum + 3):
+        webbrowser.open (webLink[i])
+
+elif timesNum == 4:
+    for i in range(searchNum + 4):
+        webLink.append(f"https://www.bing.com/search?q={(i + 5)}")
+    for i in range(searchNum + 4):
+        webbrowser.open (webLink[i])
+
+else:
+    print("You either didn't input the correct number, or a number at all.")
