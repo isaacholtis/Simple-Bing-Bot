103b7f4a6f3987a4f53aca71e33f6357c0555787
Isaac Holt <holtimzws@outlook.com>
2022-02-03 16:06:01 -0700

    Update readme.md and add basic functionality to the bot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Simple-Bing-Bot
+
+This is a simple bing rewards bot, that will currently completes bing searches to give you microsoft rewards points.
--- bingbot.py
+++ bingbot.py
@@ -1,2 +1,13 @@
 import webbrowser
 import random
+
+searchNum = int(input("Please input the number of tabs you want to open.\nPlease note that the more tabs you open the more ram you will use, and we are not responsible for your computer crashing.\n-->"))
+
+
+num = 0
+webLink = []
+for i in range(searchNum):
+    webLink.append(f"https://www.bing.com/search?q={(i + 1)}")
+for i in range(searchNum):
+    webbrowser.open (webLink[i])
+print(webLink)
