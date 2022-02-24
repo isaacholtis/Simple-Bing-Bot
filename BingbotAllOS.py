eeaea68c506029a831efb23736a00cf944c36f11
Dom Kirnite <conner@darlingappraising.com>
2022-02-24 10:17:52 -0700

    json

--- BingbotAllOS.py
+++ BingbotAllOS.py
@@ -19,13 +19,7 @@ import webbrowser
 import time
 import random
 import json
-"""info = {'email': 'conner@darlingappraising.com', 'passkey': '3Dreams@real'}
-with open('info.json', 'w') as file:
-    json.dump(info, file)
-with open('info.json', 'r') as file:
-    printer = json.load(file)
-    print(printer['email'])"""
-#check what search engine to use
+
 for i in range(5):
     print("which browser do you want the bot to use? (the browser needs to be installed) \n1. Google Chrome\n2. Microsoft Edge\n3. Mozilla Firefox")
     BrowserChoice = str(input("[1/2/3]--> "))
@@ -54,26 +48,6 @@ NEXTBUTTON = (By.ID, "idSIButton9")
 SIGNINBUTTON = (By.ID, "id_a")
 print("Welcome to Bing Bot! We need to do some setup, and then we'll be ready.")
 
-"""for i in range(5):
-    try:
-        # Loads the pickle files with the user dictionarys
-        with open('encEPwd.pkl', 'rb') as encOpenEmail:
-            encFiles = pickle.load(encOpenEmail)
-        with open('encKey.pkl', 'rb') as encOpenKey:
-            encFilesKey = pickle.load(encOpenKey)
-        break
-    except:
-        encFiles = {}
-        encFilesKey = {}
-        break
-else:
-    print("Looks like we've hit an error loading the user data base files that we can't resolve. We'll have to close the program. :(")
-    print("FATAL: .pkl file does not exist")
-    print("System will exit in 3 seconds")
-    time.sleep(3)
-    sys.exit()
-# Get a variable to tell the system how many searches each thread completes...
-# This will be double the searches intended by the user, so we need to fix that"""
 for i in range(5):
     try:
         searchNum = int(input("Please input the number of searches you want completed.\nEach search will open a tab once, and then close that tab, so feel free to do as many as you want!\n-->"))
@@ -104,131 +78,47 @@ else:
     time.sleep(3)
     sys.exit()
 
+# get username and see if user exists in the json file (database)
+makeaccount = ''
 username = input("What is your username? enter in any username you want if this is your first time with the bot or you want to create new credentials for the microsoft account to use\n--> ")
-with open('info.json', 'r') as checkfile:
-    checkfile2 = json.load(checkfile)
-    if username == checkfile2[username]:
-        reguser = "True"
+database = open('info.json', 'r')
+database2 = json.load(database)
+if username == database2['username']:
+    email = database2['email']
+    decryptkey = Fernet(bytes(database2['enckey'], 'utf-8'))
+    password = decryptkey.decrypt(bytes(database2['encpassword'], 'utf-8'))
 
-if reguser == "True":
-    with open('info.json', 'r') as infofile:
-        infofile2 = json.load(infofile)
-        email = infofile[email]
-"""# Gets the name of the user, so we can check if they have entered their credentials before
-for i in range(5):
-    try:
-        userName = input("We need your Microsoft account details so we can sign you in to save your search points.\nPlease input your first and last name so we can store you encrypted username and password.\nOr, if you don't want to save your details, there will be an option to opt out, and you can just enter them, but they won't be saved.\nIf you've already entered your email and password once, then put in your name, and we'll load your profile if it exists in our system.\n-->")
-    except ValueError:
-        print("You didn't type letters!\n\nTry again!\n")
-        continue
-    else:
-        break
-else:
-    print("You did something wrong too many times. Try rerunning the program. :(")
-    print("FATAL: unknown error")
-    print("System will exit in 3 seconds")
-    time.sleep(3)
-    sys.exit()
-# If the user has entered credentials, the system won't ask again
-try:
-    if userName + " Email" in encFiles:
-        print("You exist in our system! Logging you in now!")
+# if the username doesnt match, the program asks the user to make a new profile
 
-    # If the user has not entered credentials before, the system will ask the user to enter them
-    else:
-        enrollDetails = str.lower(input("Would you like us to save your account? This will erase any other users.\nType yes or no.\n-->"))
-        if enrollDetails == "yes":
-            for i in range(5):
-                try:
-                    email = input("Please enter your Microsoft account email.\n-->")
-                    break
-                except:
-                    print("Sorry! You typed a number, but we need a letter! Try again!")
-                    continue
-            else:
-                print("Sorry! There was an error saving your account details, so we couldn't continue!")
-                print("Exiting in 3 seconds.")
-                print("FATAL: unknown error")
-            for i in range(5):
-                try:
-                    pwd = input("Please input your Microsoft account password.\n-->")
-                    break
-                except:
-                    print("Sorry! You typed a number, but we need a letter! Try again!")
-                    continue
-            else:
-                print("Sorry! There was an error saving your account details, so we couldn't continue!")
-                print("Exiting in 3 seconds.")
-                print("FATAL: unknown error")
-            # Creates encryption keys, I think this could be simplified
-            key = Fernet.generate_key()
-            key2 = Fernet.generate_key()
-            keyEmail = Fernet(key)
-            keyPwd = Fernet(key2)
-            # Encrypts the email and password
-            encEmail = keyEmail.encrypt(email.encode())
-            encPwd = keyPwd.encrypt(pwd.encode())
-            # Dictionary that will be stored in a pickle file...
-            # with the encrypted email and password in it with appropriate dictionary...
-            #  keys to call back the email and password later
-            encEPPickle = {userName + " Email": encEmail, userName + " Password": encPwd}
-            try:
-            # Store the dictionary in a pickle file called encEPwd.pkl
-                with open('encEPwd.pkl', 'wb') as encFile:
-                    pickle.dump(encEPPickle, encFile)
-            except:
-                print("We couldn't save your profile, but we can still continue! Would you like to? Type yes or no.\n-->")
-                goOn = str.lower(input())
-                if goOn == "yes":
-                    pass
-            # Dictionary that will be stored in a pickle file...
-            #  with the decryption keys for the email and password in it with appropriate dictionary...
-            #  keys to call back the decryption keys later
-            encKeyPickle = {userName + " keyEmail": keyEmail, userName + " keyPwd": keyPwd}
-            try:
-                with open('encKey.pkl', 'wb') as encKey:
-                    pickle.dump(encKeyPickle, encKey)
-                print("Profile saved succesesfully!\nJust type your name next time you use the bot, and we'll remember you password.\nAnd of course, it also encrypted.")
-            except:
-                print("We couldn't save your profile, but we can still continue! Would you like to? Type yes or no.\n-->")
-                goOn = str.lower(input())
-                if goOn == "yes":
-                    pass
-            print("Profile saved succesesfully!\nJust type your name next time you use the bot, and we'll remember you password.\nAnd of course, it also encrypted.")
+elif username != database2['username']:
+    for i in range(5):
+        makeaccount = str(input("you're username doesn't exist in the bot's database.\nwould you like to make a new one? [Y/n]--> "))
+        if makeaccount.lower() == "y" or makeaccount.lower() == "n":
+            break
         else:
-            print("Okay! We won't save your profile!")
-            print("We'll still need your Microsoft account email and password to login though.")
-            for i in range(5):
-                try:
-                    email = input("Please enter your Microsoft account email.\n-->")
-                    break
-                except:
-                    print("Sorry! You typed a number, but we need a letter! Try again!")
-                    continue
-            else:
-                print("Sorry! There was an error while you were inputing your account details.")
-                print("Exiting in 3 seconds.")
-                print("FATAL: unknown error")
-                sys.exit(3)
-            for i in range(5):
-                try:
-                    pwd = input("Please input your Microsoft account password.\n-->")
-                    break
-                except:
-                    print("Sorry! You typed a number, but we need a letter! Try again!")
-                    continue
-            else:
-                print("Sorry! There was an error you were inputing your account details.")
-                print("Exiting in 3 seconds.")
-                print("FATAL: unknown error")
-                sys.exit(3)
-            
-except:
-    print("A fatal error occured!! We need to exit. Try rerunning the program. :(")
-    print("FATAL: unknown error")
-    print("System will exit in 3 seconds")
-    time.sleep(3)
-    sys.exit()"""
+            print("Invalid input. Type y or n.")
+
+    if makeaccount.lower() == "y":
+        newusername = input("Type the username that you want to use for this bot.\n--> ")
+        newemail = input("Type the email address that you use for your microsoft account.\n--> ")
+        insecurenewpassword = bytes(input("Type the password for your microsoft account.(it will be encrypted)\n--> "), 'utf-8')
+        newenckey = Fernet.generate_key()
+        f = Fernet(newenckey)
+        newencpassword = f.encrypt(insecurenewpassword)
+database.close()
+if makeaccount.lower() == "y":
+    with open('info.json', 'w') as savefile:
+        infodict = {'email': str(newemail), 'encpassword': str(newencpassword, 'utf-8'), 'enckey': str(newenckey, 'utf-8'), 'username': str(newusername)}
+        jsonobject = json.dumps(infodict)
+        savefile.write(jsonobject)
+    infofile = open('info.json', 'r')
+    infofile2 = json.load(infofile)
+    email = infofile2['email']
+    decryptkey = Fernet(bytes(infofile2['enckey'], 'utf-8'))
+    password = decryptkey.decrypt(bytes(infofile2['encpassword'], 'utf-8'))
+    print(password)
+    infofile.close()
+
 print("Okay! Starting login, and searches!")
 
 # Defines the funtion that the threads use later on to search...
@@ -268,7 +158,6 @@ def searchNumR(n1, n2):
                 serNum = f"{i + 1}d"
             print(f"Hmm, the {serNum} search didn't work. do you have internet? Trying again.")
             continue
-
 for i in range(5):
     try:
         # Login for the first webdriver instance
@@ -278,27 +167,25 @@ for i in range(5):
         # Click Next
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
         # Wait for password field and enter password
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(PASSWORDFIELD)).send_keys(pwd)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(PASSWORDFIELD)).send_keys(str((password), 'utf-8'))
         # Click Login
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
         # Click Yes
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
-        time.sleep(1)
+        time.sleep(5)
         break
     except:
-        print("Weird, the login didnt work. We'll try again.")
+        print("Login failed. trying again.")
         continue
 else:
-    print("There were some issues getting you logged into your account. We tried several times, but we couldn't get it to work. :(")
-    print("We'll continue on with the searches, but you won't be signed in.")
-time.sleep(4)
-# For loops to create two instances running at once
+    print("The login won't work. make sure you entered in the correct email and password for your account when you set up the bot")
+    print("We'll run the searches, but you might not get points for them. press CTRL + C or COMMAND + C to cancel.")
+time.sleep(3)
+
+#function to run the searches
 searchNumR(searchNum, timeNum)
-# Click the sign in button after the bing searches are complete...
-# Since they don't always sign in after the earlier bit of code
-time.sleep(4)
-#id = driver.find_element(By.ID, "id_a")
-#id.click()
-time.sleep(5)
+
+#wait 3 seconds and then close the program
+time.sleep(3)
 driver.close()
-print("Searches completed succsesfully! Thanks again for using our bot!")
+print("Searches completed succsesfully! Thanks for using our bot!")
